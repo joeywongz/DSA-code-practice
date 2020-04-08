6ca3f52436b4412492de52c179b3ee3309bbdeb6
wangchong <370318843@qq.com>
2020-04-09 07:35:34 +0800

    快速排序更新

--- quicksort.js
+++ quicksort.js
@@ -2,14 +2,14 @@
 
 
 /**
- * 快速排序（这种写法容易报错： RangeError: Maximum call stack size exceeded）
+ * 快速排序
  * @param {乱序数组} arr 
  */
 function quicksort(arr) {
     if (arr.length < 2) {
         return arr;
     } else {
-        let pivot = arr[0];
+        let pivot = arr.splice(0, 1)[0];    //Array.prototype.splice的返回值为去掉的元素组成的数组，并改变原有数组
         let [less, greater] = [[], []];
         arr.forEach(element => {
            element <= pivot ? less.push(element) 
