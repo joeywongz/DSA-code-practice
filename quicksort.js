69367b6b124011ed8e099f54596d5e95090ee6c5
wangchong <370318843@qq.com>
2020-04-09 00:28:31 +0800

    快速排序更新

--- quicksort.js
+++ quicksort.js
@@ -1,4 +1,10 @@
 // 快速排序，时间复杂度：O(nlogn)
+
+
+/**
+ * 快速排序（这种写法容易报错： RangeError: Maximum call stack size exceeded）
+ * @param {乱序数组} arr 
+ */
 function quicksort(arr) {
     if (arr.length < 2) {
         return arr;
@@ -10,5 +16,21 @@ function quicksort(arr) {
            : greater.push(element);
         });
         return [...quicksort(less), pivot, ...quicksort(greater)];
+    }
+}
+
+
+
+function quickSort(arr) {
+    if (arr.length < 2) {
+        return arr;
+    } else {
+        let pivot = arr.splice(0, 1)[0];
+        let [less, greater] = [[], []];
+        arr.forEach(element => {
+           element <= pivot ? less.push(element) 
+           : greater.push(element);
+        });
+        return quickSort(less).concat[[pivot], quickSort(greater)];
     }
 }
