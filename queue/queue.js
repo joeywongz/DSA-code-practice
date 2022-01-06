42be3ce1b1363a83f5f19808f532240c742cc8e1
Joey Wong <370318843@qq.com>
2022-01-06 20:38:44 +0800

    实现队列

---
+++ queue/queue.js
@@ -0,0 +1,54 @@
+export class Queue {
+  constructor() {
+    this.items = {}
+    this.count = 0
+    this.lowestCount = 0
+  }
+  enqueue(element) {
+    this.items[this.count] = element
+    this.count++
+  }
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    const result = this.items[this.lowestCount]
+    delete this.items[this.lowestCount]
+    this.lowestCount++
+    return result
+  }
+  isEmpty() {
+    return this.size() === 0
+  }
+  peek() {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    return this.items[this.lowestCount]
+  }
+  size() {
+    return this.count - this.lowestCount
+  }
+  clear() {
+    this.items = {}
+    this.count = 0
+    this.lowestCount = 0
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return ''
+    }
+    let objString = `${this.items[this.lowestCount]}`
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`
+    }
+    return objString
+  }
+}
+
+const queue = new Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+// queue.dequeue()
+console.log(queue.toString())
+console.log(queue.size())
